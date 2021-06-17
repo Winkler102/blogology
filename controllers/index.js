d09d24a7dc226428542e330432933a4fd59d705d
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-17 18:51:21 -0500

    added basic home page

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
+
 // const dashboardRoutes = require('./dashboard-routes.js');
-
 const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./home-routes.js');
 
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 // router.use('/dashboard', dashboardRoutes);
 
