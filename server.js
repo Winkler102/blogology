9dd3f8ae644adeecd950984a10b46e69bcf9cd73
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-17 12:57:49 -0500

    added routes for users and posts

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const path = require('path');
+const routes = require('./controllers');
+const helpers = require('./utils/helper');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'ao;dfiawoeij asiopfawe;i awepofk aweopk ',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
