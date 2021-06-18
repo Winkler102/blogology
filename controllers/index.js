9caac71494fba9d3d3d9a3d98e6bdf9c81bfdd1c
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-18 12:25:41 -0500

    got dashobaord displayed

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
-// const dashboardRoutes = require('./dashboard-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/dashboard', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
