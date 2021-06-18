554f8bc40849c3bdda6736ace47f4ef1fc8edae7
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-18 15:47:57 -0500

    update

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,6 +47,15 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
 router.get('/post/:id', (req, res) => {
     Post.findOne({
         where: {
@@ -54,10 +63,9 @@ router.get('/post/:id', (req, res) => {
         },
         attributes: [
             'id',
-            'post_url',
+            'text',
             'title',
             'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
             {
